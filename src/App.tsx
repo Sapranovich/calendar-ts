a8829aa08e2f7ea07669921456eff83a0c290696
Andrey Sapranovich <SapranovichAndrey@yandex.ru>
2021-01-11 14:39:59 +0300

    feat: build basic dependencies in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
 import React from 'react';
 
-import Calendar from './components/Calendar';
-import Modal from './components/Modal';
-import ToDoList from './components/ToDoList';
-
-import {IStandartTypesStore} from './interfaces/basicInterfaces';
-
 function App() {
-  const date = new Date();
-  const currentDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-
-  const [store, setStore] = React.useState<IStandartTypesStore>({
-    isOpenModal: false,
-    calendarDate: currentDate,
-    currentDate: currentDate,
-    selectedDate: currentDate,
-    idSelectedDate: currentDate.getTime(),
-    storeMessages: JSON.parse(localStorage.getItem("storeMessages") || '{}'),
-    dataChangeMessage: {
-       currentHour: undefined,
-       text: '',
-     },
-  })
-
   return (
-    <div className="app">
-      <div className="wrapper">
-        <div className="app__wrapper">
-          <Calendar  store={store} setStore={setStore}/>
-          <ToDoList store={store} setStore={setStore}/>
-        </div>
-        {store.isOpenModal && <Modal store={store} setStore={setStore}/> }
-      </div>
+    <div className="App">
+      asdfsdfsd
     </div>
   );
 }
